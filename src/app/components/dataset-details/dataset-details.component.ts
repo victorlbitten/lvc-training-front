7ff45973505a6cef43d4e44f1ca452c1aedecbc1
victorlbitten <victorlbittenc@gmail.com>
2024-03-12 14:10:36 -0300

    Now we can delete datasets

--- src/app/components/dataset-details/dataset-details.component.ts
+++ src/app/components/dataset-details/dataset-details.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { DatasetName } from 'src/app/models/datasets.model';
+import { DatasetService } from 'src/app/services/dataset.service';
 
 @Component({
   selector: 'app-dataset-details',
   templateUrl: './dataset-details.component.html',
-  styleUrls: ['./dataset-details.component.css']
+  styleUrls: ['./dataset-details.component.css'],
 })
-export class DatasetDetailsComponent {
+export class DatasetDetailsComponent implements OnInit {
+  datasetName: DatasetName;
 
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private datasetService: DatasetService
+  ) {}
+
+  ngOnInit() {
+    this.datasetName = this.route.snapshot.params['datasetname'];
+  }
+
+  deleteDataset() {
+    this.datasetService.deleteDataset(this.datasetName).subscribe(() => {
+      this.router.navigate(['/datasets']);
+    });
+  }
 }

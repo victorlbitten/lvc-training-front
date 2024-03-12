9b2010f7c1463e920ccc1af27e83dd89bf033f4a
victorlbitten <victorlbittenc@gmail.com>
2024-03-12 13:07:33 -0300

    Creating datasetDetails.component

---
+++ src/app/components/dataset-details/dataset-details.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dataset-details',
+  templateUrl: './dataset-details.component.html',
+  styleUrls: ['./dataset-details.component.css']
+})
+export class DatasetDetailsComponent {
+
+}
